36867d2f63ca5d8f2d17a9e81067d38d21c10433
Ed <edwinli123456@gmail.com>
2024-11-09 03:14:45 +1100

    create skeleton project with minecraft development plugin

---
+++ src/main/kotlin/dev/edwnl/macSMPCore/MacSMPCore.kt
@@ -0,0 +1,14 @@
+package dev.edwnl.macSMPCore
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MacSMPCore : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
