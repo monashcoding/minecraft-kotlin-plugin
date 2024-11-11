3b5f13dc75b9fb17c05db9fc407a0c66c57c16b8
Ed <edwinli123456@gmail.com>
2024-11-12 00:11:42 +1100

    give 16 steak on first join

--- src/main/kotlin/dev/edwnl/macSMPCore/listeners/PlayerLoginListener.kt
+++ src/main/kotlin/dev/edwnl/macSMPCore/listeners/PlayerLoginListener.kt
@@ -3,10 +3,13 @@ package dev.edwnl.macSMPCore.listeners
 import dev.edwnl.macSMPCore.database.MongoDB
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent
+import org.bukkit.event.player.PlayerLoginEvent
+import org.bukkit.inventory.ItemStack
 
 class PlayerLoginListener(private val mongoDB: MongoDB) : Listener {
 
@@ -18,6 +21,20 @@ class PlayerLoginListener(private val mongoDB: MongoDB) : Listener {
                 Component.text("You are not whitelisted. Use /whitelist add ${event.playerProfile.name} on the MAC discord!")
                     .color(NamedTextColor.RED)
             )
+        }
+    }
+
+    @EventHandler
+    fun onPlayerLogin(event: PlayerLoginEvent) {
+        val player = event.player
+
+        // Check if this is the player's first time joining
+        if (!player.hasPlayedBefore()) {
+            // Create starter food package
+            val starterFood = ItemStack(Material.COOKED_BEEF, 16)
+
+            // Add items to player's inventory
+            player.inventory.addItem(starterFood)
         }
     }
 }
